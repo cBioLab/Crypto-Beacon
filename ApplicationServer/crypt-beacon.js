03d6ba153d173f1525926b6cc0e3a179220683df
jimjin73 <jimwase@asagi.waseda.jp>
2019-02-25 15:07:30 +0900

    init

---
+++ ApplicationServer/crypt-beacon.js
@@ -0,0 +1,364 @@
+let sec = null
+let pub = null
+let ppub = null
+let oneG1 = null
+let zkp_oneG1 = null
+let oneG2 = null
+let zkp_oneG2 = null
+let vecG1 = []
+let zkp_vecG1 = []
+let vecG2 = []
+let zkp_vecG2 = []
+let cG1 = null
+let cGT = null
+let chr = 0
+let chrlen = 0
+let posG1 = 0
+let posG2 = 0
+let posGT = 0
+let pos = 0
+const chrnum = {}
+const nt2i ={}
+const chrnt = []
+let nt = 0
+let position = 0
+let total_len = 0
+let chrflag = true
+let HEflag = true
+let lengthX = 0
+let lengthY = 0
+let posX = 0
+let posY = 0
+const num_of_database = 4
+
+she.init(0).then(() => {
+    document.getElementsByName("status")[0].innerText = "Initializing query..."
+    setTimeout(()=>{
+	setinfo()
+	sec = new she.SecretKey()
+	sec.setByCSPRNG()
+	sec.dump('sec=')
+	pub = sec.getPublicKey()
+	pub.dump('pub=')
+	document.getElementsByName("status")[0].innerText = "Loading table..."
+	setTimeout(()=>{
+	    ppub = new she.PrecomputedPublicKey()
+	    ppub.init(pub)
+	    document.getElementsByName("status")[0].innerText = 'Fill in the form below and click "Send an Encrypted Beacon" button.'
+	    document.getElementsByName("sendbutton")[0].disabled = false
+	    document.getElementsByName("query-form")[0].style.display = "block" 
+	},100);
+    },100);
+})
+
+function setinfo(){
+    chrnum.chr1 = 1
+    chrnum.chr2 = 2
+    chrnum.chr3 = 3
+    chrnum.chr4 = 4
+    chrnum.chr5 = 5
+    chrnum.chr6 = 6
+    chrnum.chr7 = 7
+    chrnum.chr8 = 8
+    chrnum.chr9 = 9
+    chrnum.chr10 = 10
+    chrnum.chr11 = 11
+    chrnum.chr12 = 12
+    chrnum.chr13 = 13
+    chrnum.chr14 = 14
+    chrnum.chr15 = 15
+    chrnum.chr16 = 16
+    chrnum.chr17 = 17
+    chrnum.chr18 = 18
+    chrnum.chr19 = 19
+    chrnum.chr20 = 20
+    chrnum.chr21 = 21
+    chrnum.chr22 = 22
+    chrnum.chrX = 23
+    chrnum.chrY = 24
+    chrnum.chrM = 25
+
+    nt2i.A = 0
+    nt2i.C = 1
+    nt2i.G = 2
+    nt2i.T = 3
+
+    chrnt.push(0) 
+    chrnt.push(249250621) //1
+    chrnt.push(243199373) //2
+    chrnt.push(198022430) //3
+    chrnt.push(191154276) //4
+    chrnt.push(180915260) //5
+    chrnt.push(171115067) //6
+    chrnt.push(159138663) //7
+    chrnt.push(146364022) //8
+    chrnt.push(141213431) //9
+    chrnt.push(135534747) //10
+    chrnt.push(135006516) //11
+    chrnt.push(133851895) //12
+    chrnt.push(115169878) //13
+    chrnt.push(107349540) //14
+    chrnt.push(102531392) //15
+    chrnt.push(90354753) //16
+    chrnt.push(81195210) //17
+    chrnt.push(78077248) //18
+    chrnt.push(59128983) //19
+    chrnt.push(63025520) //20
+    chrnt.push(48129895) //21
+    chrnt.push(51304566) //22
+    chrnt.push(155270560) //X
+    chrnt.push(59373566) //Y
+    chrnt.push(1000000) //M
+
+    total_len = 3096677412
+}
+
+function enc(){
+    if(HEflag){
+	let [c , zkp] = ppub.encWithZkpBinG1(1)
+	oneG1 = window.btoa(String.fromCharCode.apply(null,c.serialize()))
+	zkp_oneG1 = window.btoa(String.fromCharCode.apply(null,zkp.serialize()))
+	for(let i = 0;i<lengthX;i++){
+	    [c , zkp] = ppub.encWithZkpBinG1(0)
+	    vecG1.push(btoa(String.fromCharCode.apply(null,c.serialize())))
+	    zkp_vecG1.push(btoa(String.fromCharCode.apply(null,zkp.serialize())))
+	}
+	[c , zkp] = ppub.encWithZkpBinG2(1)
+	oneG2 = btoa(String.fromCharCode.apply(null,c.serialize()))
+	zkp_oneG2 = window.btoa(String.fromCharCode.apply(null,zkp.serialize()))
+	for(let i = 0;i<lengthY;i++){
+	    [c , zkp] = ppub.encWithZkpBinG2(0)
+	    vecG2.push(btoa(String.fromCharCode.apply(null,c.serialize())))
+	    zkp_vecG2.push(btoa(String.fromCharCode.apply(null,zkp.serialize())))
+	}
+	console.log("vecG1 len" + vecG1.length)
+	console.log("vecG2 len" + vecG2.length)
+    }else{
+	let [c , zkp] = ppub.encWithZkpBinG1(1)
+	oneG1 = btoa(String.fromCharCode.apply(null,c.serialize()))
+	zkp_oneG1 = window.btoa(String.fromCharCode.apply(null,zkp.serialize()))
+	for(let i = 0;i<lengthX;i++){
+	    [c , zkp] = ppub.encWithZkpBinG1(0)
+	    vecG1.push(btoa(String.fromCharCode.apply(null,c.serialize())))
+	    zkp_vecG1.push(btoa(String.fromCharCode.apply(null,zkp.serialize())))
+	}
+	console.log("vecG1 len" + vecG1.length)
+    }
+}
+
+function calcPos(){
+    //chr,pos,NTから位置を計算
+    chr = chrnum[document.getElementsByName("chromosome")[0].value]
+    position = document.getElementsByName("position")[0].value
+    nt = nt2i[document.getElementsByName("nucleotide")[0].value]
+    console.log("chr : " + chr)
+    console.log("position : " + position)
+    console.log("nucleotide : " + nt)
+    if(chr == null) return 1 
+    if(position == null || position == '') return 2 
+    if(nt == null) return 3
+    if(isFinite(position) == false || position < 0 || position > parseInt(chrnt[chr])) return 4 
+    return 0 
+}
+
+function calcEachPos(){
+    let x
+    let y
+    chrflag = document.check.private.checked //染色体公開オプション用
+    HEflag = chrflag //true : SHE, false : AHE ←染色体公開するかとHEの切り替えをわけるならここ
+    console.log("chrflag : " + chrflag)
+    if(chrflag){//true : 全ゲノム, false : 染色体選択
+	let tmp = 0
+	for(let i=0;i<chr;i++){
+	    tmp += parseInt(chrnt[i])
+	}
+	pos = (parseInt(position) + parseInt(tmp)) * 4 + nt
+    }else{
+	pos = position * 4 + nt
+    }
+    console.log("pos : " + pos)
+    if(HEflag){
+	let mid = Math.ceil(Math.pow(Math.ceil(total_len / 18),(1/3)))
+	lengthX = 12 * mid
+	lengthY = 6 * mid
+	console.log("lengthG1 : " + lengthX)
+	console.log("lengthG2 : " + lengthY)
+	posG1 = pos % lengthX
+	posG2 = (pos / lengthX | 0) % lengthY
+	posGT = pos / (lengthX*lengthY) | 0
+    }else{
+	lengthX = 10000
+	lengthY = Math.ceil(parseInt(chrnt[chr]) * 4 / lengthX)
+	console.log("lengthX : " + lengthX)
+	console.log("lengthY : " + lengthY)
+	posX = pos % lengthX
+	posY = pos / lengthX | 0
+    }
+}
+
+function sort(){
+    if(HEflag){
+	console.log("posG1 : " + posG1)
+	console.log("posG2 : " + posG2)
+	console.log("posGT : " + posGT)
+	vecG1.splice(posG1,1,oneG1)
+	zkp_vecG1.splice(posG1,1,zkp_oneG1)
+	vecG2.splice(posG2,1,oneG2)
+	zkp_vecG2.splice(posG2,1,zkp_oneG2)
+	cGT = btoa(String.fromCharCode.apply(null,ppub.encGT(posGT).serialize()))
+	console.log("vecG1 len " + vecG1.length)
+	console.log("vecG2 len " + vecG2.length)
+    }else{
+	console.log("posX : " + posX)
+	console.log("posY : " + posY)
+	vecG1.splice(posX,1,oneG1)
+	zkp_vecG1.splice(posX,1,zkp_oneG1)
+	cG1 = btoa(String.fromCharCode.apply(null,ppub.encG1(posY).serialize()))
+	console.log("vecG1 len " + vecG1.length)
+	//console.log(cG1)
+    }
+}
+
+function gettime(){
+    var Nowymdhms　=　new Date();
+    var NowYear = Nowymdhms.getYear() + 1900;
+    var NowMon = Nowymdhms.getMonth() + 1;
+    var NowDay = Nowymdhms.getDate();
+    var NowWeek = Nowymdhms.getDay();
+    var NowHour = Nowymdhms.getHours();
+    var NowMin = Nowymdhms.getMinutes();
+    var NowSec = Nowymdhms.getSeconds();
+    myMsg = NowYear+"年"+NowMon+"月"+NowDay+"日"+NowHour+"時"+NowMin+"分"+NowSec+"秒";
+    console.log(myMsg);
+}
+
+function getdevice(){
+    var ua = navigator.userAgent;
+    device = 'pc'
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0){
+        device = 'iPhone';
+    }else if(ua.indexOf('Android') > 0){
+        device = 'Android';
+    }
+}
+
+function post() {
+    console.log("post")
+    document.getElementsByName("status")[0].innerText = "Now searching..."
+    if(chrflag) chr = 0
+    gettime()
+    $.ajax({
+	//url: 'https://' + location.host + '/cb/api',
+	url: 'http://venus.cbio.cs.waseda.ac.jp:3000',
+        type: 'POST',
+	data: {
+	    'pub': btoa(String.fromCharCode.apply(null,pub.serialize())),
+	    'vecG1': vecG1,
+	    'vecG2': vecG2,
+	    'zkp_vecG1': zkp_vecG1,
+	    'zkp_vecG2': zkp_vecG2,
+	    'cG1': cG1,
+	    'cGT': cGT,
+	    'chrflag': chrflag,
+	    'HEflag': HEflag,
+	    'chr': chr
+	},
+        dataType: 'json',
+    }).done(function( data, textStatus, jqXHR ) {
+        console.log(data)
+	gettime()
+	dec(data)
+    }).fail(function( jqXHR, textStatus, errorThrown) {
+        console.log("error")
+	document.getElementsByName("status")[0].innerText = "Search failed"
+    }).always(function( jqXHR, textStatus) {
+        console.log("fin")
+	vecG1 = []
+	vecG2 = []
+	zkp_vecG1 = []
+	zkp_vecG2 = []
+	cG1 = null
+	cGT = null
+	document.getElementsByName("sendbutton")[0].disabled = false
+    });
+}
+
+function send(){
+    console.log("send")
+    for(let i=0;i<num_of_database;i++){
+	document.getElementsByName("found")[i].innerText = "-"
+	document.getElementsByName("found")[i].style.backgroundColor = "white"
+	document.getElementsByName("found")[i].style.color = "black"
+	document.getElementsByName("found")[i].style.padding = "5px"
+    }
+    var retcode = calcPos()
+    var msg = { 1 : "Select Chromosome", 2 : "Enter Base Position", 3 : "Select Alternative Allele" }
+    if(retcode == 0){
+        calcEachPos()
+	getdevice()
+	if(device == 'iPhone'){
+	    document.getElementsByName("status")[0].innerText = "Encrypting query... (This may take 10 to 25 sec for your device (iPad or iPhone))"
+	}else if(device == 'Android'){
+	    document.getElementsByName("status")[0].innerText = "Encrypting query... (This may take 30 to 60 sec for your device (Android device) )"
+	}else{
+	    document.getElementsByName("status")[0].innerText = "Encrypting query..."
+	}
+        document.getElementsByName("sendbutton")[0].disabled = true
+	setTimeout(()=>{
+            enc()
+            sort()
+            post()
+	},100)
+    }else if(retcode == 4){
+        console.log("input error")
+        let chr = chrnum[document.getElementsByName("chromosome")[0].value]
+        let maxposition = chrnt[chr]
+        document.getElementsByName("status")[0].innerText = "Enter Vaild Base Position (0 < position <= " + maxposition + ")"
+    }else{
+        console.log("input error")
+        document.getElementsByName("status")[0].innerText = msg[retcode] 
+    }
+}
+
+function dec(data) {
+    if(data.status == "ok"){
+	let result = [null,null,null,null]
+	let ct = [null,null,null,null]
+
+	if(HEflag){
+	    for(let i=0;i<num_of_database;i++) ct[i] = new she.CipherTextGT()
+	    ct[0].deserialize(new Uint8Array(atob(data.vecAns0[posGT]).split("").map(function(c) {return c.charCodeAt(0); })))
+	    ct[1].deserialize(new Uint8Array(atob(data.vecAns1[posGT]).split("").map(function(c) {return c.charCodeAt(0); })))
+	    ct[2].deserialize(new Uint8Array(atob(data.vecAns2[posGT]).split("").map(function(c) {return c.charCodeAt(0); })))
+	    ct[3].deserialize(new Uint8Array(atob(data.vecAns3[posGT]).split("").map(function(c) {return c.charCodeAt(0); })))
+	}else{
+	    for(let i=0;i<num_of_database;i++) ct[i] = new she.CipherTextG1()
+	    ct[0].deserialize(new Uint8Array(atob(data.vecAns0[posY]).split("").map(function(c) {return c.charCodeAt(0); })))
+	    ct[1].deserialize(new Uint8Array(atob(data.vecAns1[posY]).split("").map(function(c) {return c.charCodeAt(0); })))
+	    ct[2].deserialize(new Uint8Array(atob(data.vecAns2[posY]).split("").map(function(c) {return c.charCodeAt(0); })))
+	    ct[3].deserialize(new Uint8Array(atob(data.vecAns3[posY]).split("").map(function(c) {return c.charCodeAt(0); })))
+	}
+	for(let i=0;i<num_of_database;i++){
+	    result[i] = sec.dec(ct[i])
+	    let ans = null
+	    if(result[i] == 1){
+		document.getElementsByName("found")[i].innerText = "Found"
+		document.getElementsByName("found")[i].style.backgroundColor = "#32cd32"
+		document.getElementsByName("found")[i].style.color = "white"
+	    }else{
+		document.getElementsByName("found")[i].innerText = "Not Found"
+		document.getElementsByName("found")[i].style.backgroundColor = "#dc143c"
+		document.getElementsByName("found")[i].style.color = "white"
+	    }
+	}
+	document.getElementsByName("status")[0].innerText = "Search done"
+    }else{
+	document.getElementsByName("status")[0].innerText = "Search failed"
+    }
+}
+
+window.onbeforeunload = function(e){
+    ppub.destroy()
+    console.log("exit")
+};
+
---
+++ WebServer/she_server.js
@@ -0,0 +1,110 @@
+var bodyParser = require('body-parser')
+var express = require('express')
+var morgan = require('morgan')
+const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
+const fs = require('fs')
+var app = express()
+
+app.use(express.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit:'50mb',extended: true,parameterLimit: 1000000 }));
+
+app.use(morgan('combined'));
+app.use(morgan({ format: 'dev', immediate: true }));
+
+app.post('/', function (req, res){
+    let vecG1 = null
+    let vecG2 = null
+    let zkp_vecG1 = null
+    let zkp_vecG2 = null
+    let cGT = null
+    let cG1 = null
+    let pub = req.body.pub
+    let chrflag = req.body.chrflag
+    let HEflag = req.body.HEflag
+    let isPrivate = 1
+    let isSHE = 1
+    let chr = req.body.chr
+    let query = true
+
+    if(chrflag == "true"){
+	isPrivate = 1
+	if(chr != 0){
+	    console.log("error chr != 0")
+	    query = false
+	}
+    }else if(chrflag == "false"){
+	isPrivate = 0
+	if(chr <= 0 || chr >= 26){
+	    console.log("error chr?")
+	    query = false
+	}
+    }else{
+	console.log("error chrflag")
+	query = false
+    }
+
+    if(HEflag == "true"){
+	vecG1 = req.body.vecG1
+ 	zkp_vecG1 = req.body.zkp_vecG1
+	vecG2 = req.body.vecG2
+	zkp_vecG2 = req.body.zkp_vecG2
+	cGT = req.body.cGT
+	isSHE = 1
+	input = String(chr) + "\n" + String(isPrivate) + "\n"+ String(isSHE) + "\n" + pub + "\n" + vecG1.join("\n") + "\n" + vecG2.join("\n") + "\n" + cGT + "\n" + zkp_vecG1.join("\n") + "\n" + zkp_vecG2.join("\n")
+    }else if(HEflag == "false"){
+	vecG1 = req.body.vecG1
+	zkp_vecG1 = req.body.zkp_vecG1
+	cG1 = req.body.cG1
+	isSHE = 0
+	input = String(chr) + "\n" + String(isPrivate) + "\n"+ String(isSHE) + "\n" + pub + "\n" + vecG1.join("\n") + "\n" + cG1 + "\n" + zkp_vecG1.join("\n")
+    }else{
+	console.log("error HEflag")
+	query = false
+    }
+
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+
+    if(query){
+	filename = getUniqueStr() + ".dat"
+	fs.writeFileSync("./tmp/" + filename , input)
+	console.log("start exec");
+	exec('./she_server_core ../tmp/' + filename,{cwd:"./bin"},(err, stdout, stderr) => {
+	    if(err){
+		console.log("error exec core")
+		console.log(err)
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({'status':"error"}));
+	    }else{
+		console.log("fin exec");
+		try{
+		    let vecAns0 = execSync('cat ' + stdout + '0',{cwd:"./bin",maxBuffer :500*1024*1024}).toString().split('\n')
+		    let vecAns1 = execSync('cat ' + stdout + '1',{cwd:"./bin",maxBuffer :500*1024*1024}).toString().split('\n')
+		    let vecAns2 = execSync('cat ' + stdout + '2',{cwd:"./bin",maxBuffer :500*1024*1024}).toString().split('\n')
+		    let vecAns3 = execSync('cat ' + stdout + '3',{cwd:"./bin",maxBuffer :500*1024*1024}).toString().split('\n')
+		    res.writeHead(200, { 'Content-Type': 'application/json' });
+		    res.end(JSON.stringify({'status':"ok" ,'vecAns0': vecAns0,'vecAns1': vecAns1,'vecAns2': vecAns2,'vecAns3': vecAns3}));
+		}catch(err){
+		    console.log("error cat files")
+		    console.log(err)
+		    res.writeHead(200, { 'Content-Type': 'application/json' });
+		    res.end(JSON.stringify({'status':"error"}));
+		}
+	    }
+	});
+    }else{
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.end(JSON.stringify({'status':"error"}));
+    }
+})
+
+var server = app.listen(process.env.PORT || 3000)
+// See https://nodejs.org/api/http.html#http_server_timeout
+server.timeout = 600000 //10分
+
+function getUniqueStr(){
+ var rlen = 100000;
+ return new Date().getTime().toString(16)  + Math.floor(rlen*Math.random()).toString(16)
+}
